0db01aaf87b054ed85c43ea5c3f5bcdf89eb5421
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-16 17:37:25 +0100

    feat: get notes from supabase as public

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,12 +1,23 @@
 import Note from "./Note";
 
+import { supabase } from "@supabase/index";
+let { data: notes, error } = await supabase.from("notes").select("*");
+
 const Notes = () => {
+  console.log(notes);
   return (
     <section className="py-6">
-      <h1 className="text-4xl font-bold">My Notes</h1>
-      <div className="notes mt-8">
-        <Note />
+      <h1 className="text-4xl font-bold my-2">My Notes</h1>
+      <div className="notes flex gap-3 my-8">
+        {notes &&
+          !error &&
+          notes.map((data, i) => (
+            <div key={i}>
+              <Note data={data} />
+            </div>
+          ))}
       </div>
+      <h2 className="text-4xl font-bold my-5">Shared Notes</h2>
     </section>
   );
 };
