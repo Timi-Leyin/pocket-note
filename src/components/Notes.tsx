d38dcb11f7d2e129acf4b19b7e6f37f85ded9eca
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-25 00:08:10 +0100

    feat: fetch from supabase

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,21 +1,28 @@
 import Note from "./Note";
-
-import { supabase } from "@/supabase/index";
-let { data: notes, error } = await supabase.from("notes").select("*");
+import { getMyNotes } from "@/actions/notes";
+import {useLoader} from "@/hooks/index";
+import Loading from '@/components/Loading'
+import { Key } from "react";
+import { NoteProps } from "../interfaces";
 
 const Notes = () => {
-  console.log(notes);
+  const {loading, error, data} = useLoader(getMyNotes())
+  console.log(error)
   return (
     <section className="py-6">
       <h1 className="text-4xl font-bold my-2">My Notes</h1>
       <div className="notes flex gap-3 my-8">
-        <Note data={""} />
-        {/* {notes &&
-          !error &&
-          notes.map((data, i) => (
-            <div key={i}>
+        {loading && (
+          <Loading />
+        )}
+        {data && data.map((note: NoteProps, i: Key) => (
+          <div key={i}>
+          <Note data={note} />
             </div>
-          ))} */}
+          ))}
+          {error && (
+            <p>{error}</p>
+          )}
       </div>
       <h2 className="text-4xl font-bold my-5">Shared Notes</h2>
     </section>
