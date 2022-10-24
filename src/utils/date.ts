1f8c04ff382d0cb5d70d58c524751d841001ae63
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-25 00:07:19 +0100

    fix: date logic

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -12,17 +12,12 @@ const month = [
   "November",
   "December",
 ];
-export default (by = "2022-10-16T14:43:10+0:00") => {
-  const splits = by.split("T");
-  const date = splits[0];
-  const time = splits[1].split("+")[0];
-  const zone = splits[1].split("+")[1];
 
-  console.log(date, time, zone);
-  //   date
-  const _date = date.split("-");
-  const y = _date[0];
-  const m = month[Number(_date[1]) - 1];
-  const d = _date[1];
-  console.log(y, m, d);
-};
+export default (str:string)=>{
+  const date =  new Date(str)
+ const mm = month[date.getMonth()+1]
+ const dd = date.getUTCDate()
+ const yy = date.getFullYear()
+
+ return `${mm} ${dd}, ${yy}`
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export {default as date} from './date'
+export {default as format} from './format'
