7173fb64b27501f60856747f6a25a4f8649cb581
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-29 18:20:12 +0100

    feat: display logged in user avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import { HeaderProps } from "@/interfaces/index";
 import { Google, SearchNormal1, TicketDiscount } from "iconsax-react";
 import Search from "./Search";
-import {signin} from "@/actions/auth"
+import { signin } from "@/actions/auth";
 import { useLoader } from "@/hooks/index";
 import { currentUser } from "@/actions/user";
+import Loading from "./Loading";
 
 const Header = ({ title }: HeaderProps) => {
-  const {loading, error, data} = useLoader(currentUser())
-  console.log(error, loading, data)
+  const { loading, error, data } = useLoader(currentUser());
+  console.log(error, loading, data);
   return (
     <header className="flex items-center px-2 justify-between p-3 w-full">
       {/* search */}
@@ -21,11 +22,31 @@ const Header = ({ title }: HeaderProps) => {
         </h3>
       )}
 
-      {/*  */}
-      <button className="p-2 bg-red-500 px-7 rounded-full gap-3 flex-center text-xs" onClick={()=> signin()}>
-        Sign in <Google variant="Bold" size="16px" />
-      </button>
+      {/*  display loading*/}
+      <div className="">
+        {loading ? (
+          <Loading />
+        ) : data ? (
+          <div className="flex gap-1">
+            {/* show avatar if loading = false && data exists */}
+            <img
+              src={data?.avatar_url}
+              className="w-[50px] h-[50px] ring-2 select-none pointer-events-none ring-gray-100 rounded-full object-cover"
+              alt={data?.email}
+            />
+          </div>
+        ) : (
+          // show sign in button if loading = false && there is no current logged user
+          <button
+            className="p-2 bg-red-500 px-7 rounded-full gap-3 flex-center text-xs"
+            onClick={() => signin()}
+          >
+            Sign in <Google variant="Bold" size="16px" />
+          </button>
+        )}
+      </div>
     </header>
   );
 };
+
 export default Header;
