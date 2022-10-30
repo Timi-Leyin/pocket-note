5a2f15ef7105285ec91e6d1c61f6687fd7a982a2
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-30 15:27:04 +0100

    feat: added types for save function

--- src/interfaces/index.d.ts
+++ src/interfaces/index.d.ts
@@ -1,11 +1,21 @@
+import { SyntheticEvent, MutableRefObject } from "react";
+
 export interface BodyProps {
   title: string;
   children?: any;
 }
 
+export interface SaveProps {
+  title:string;
+  type?: "html"
+  user_id:string;
+  ref:MutableRefObject<HTMLDivElement>
+}
+
 
 export interface HeaderProps {
   title?: string;
+  onTitleChange?:(e:SyntheticBEvent)=> void
 }
 
 export interface NoteProps {
