183519aba5f908c450c12cc0034b259b0be50531
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-30 15:27:33 +0100

    feat: logic to save note

--- src/actions/save.ts
+++ src/actions/save.ts
@@ -1,4 +1,6 @@
-import { MutableRefObject } from "react"
+
+import {supabase} from "@/supabase/index"
+import type { SaveProps } from "../interfaces"
 // import jwt from "jsonwebtoken"
 export const save = (html:string)=>{
 
@@ -6,11 +8,17 @@ export const save = (html:string)=>{
 
 
 // draft - offline
-export const saveDraft = (ref:MutableRefObject<HTMLDivElement>)=>{
+export const saveDraft = async({title,type="html",ref,user_id}:SaveProps)=>{
     if(ref.current){
         // remove script tag
-    const token =btoa(ref.current.innerHTML)
-        console.log(token)
+        const html =btoa(ref.current.innerHTML);
+
+        await supabase.from("notes").insert([{
+            title,
+            type,
+            note:html,
+            user_id
+        }])
         return 
     }
 }
