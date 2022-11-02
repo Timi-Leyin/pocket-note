4d2b5c5aa76d3380f3f60698db42001dae8e80f6
Timileyin <timileyinoyelekan11@gmail.com>
2022-11-02 21:18:45 +0100

    feat: filter note results by user_id

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -1,7 +1,7 @@
 import { supabase } from "../supabase";
 
 export const getMyNotes = async ():Promise<[any, any[]]> =>{
-    let { data: notes, error } = await supabase.from("notes").select("*");
+    let { data: notes, error } = await supabase.from("notes").select("*").eq("user_id",atob(localStorage.getItem("sid") as string))
 
     return [error, notes as any[]]
 }
