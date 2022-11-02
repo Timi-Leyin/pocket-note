21d8b84acf990103626197063d193c0eb48b05bc
Timileyin <timileyinoyelekan11@gmail.com>
2022-11-02 21:20:36 +0100

    feat: save user_id to localstorage

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -2,6 +2,6 @@ import { supabase } from "@/supabase/index";
 
 export const currentUser = async ():Promise<[any, any]> =>{
     let { data: user, error } = await  supabase.auth.getUser();
-
+ user.user &&  localStorage.setItem("sid", btoa(user.user.id))
     return [error, user.user?.user_metadata]
 }
