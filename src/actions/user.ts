bc8f7cc63ad55338621671875a41172ca5775b2a
Timileyin <timileyinoyelekan11@gmail.com>
2022-11-03 11:50:10 +0100

    fix: removed auth id from localstorage

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -2,6 +2,5 @@ import { supabase } from "@/supabase/index";
 
 export const currentUser = async ():Promise<[any, any]> =>{
     let { data: user, error } = await  supabase.auth.getUser();
- user.user &&  localStorage.setItem("sid", btoa(user.user.id))
-    return [error, user.user?.user_metadata]
+    return [error, user.user]
 }
