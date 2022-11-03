be20dd1ff8d6456d7d934dc2f7aa7a807f0e4757
Timileyin <timileyinoyelekan11@gmail.com>
2022-11-03 11:46:46 +0100

    feat: filter notes by userId and get shared notes from supabase

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -1,7 +1,19 @@
 import { supabase } from "../supabase";
+import { currentUser } from "./user";
 
+const current = await currentUser()
 export const getMyNotes = async ():Promise<[any, any[]]> =>{
-    let { data: notes, error } = await supabase.from("notes").select("*").eq("user_id",atob(localStorage.getItem("sid") as string))
+    // fix: null readings
+    let { data: notes, error } = await supabase.from("notes").select("*").eq("user_id",current[1].id)
 
     return [error, notes as any[]]
 }
+
+
+export const getSharedNotes = async ():Promise<[any, any[]]> =>{
+    // fix: null readings
+let { data: notes, error } =  await supabase.from("notes").select("*").contains("shared", [current[1].email])
+
+    return [error, notes as any[]]
+}
+
