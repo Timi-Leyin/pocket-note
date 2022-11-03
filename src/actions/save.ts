83e57e3378d3f2c2e0d5906e0c1387051c06d498
Timileyin <timileyinoyelekan11@gmail.com>
2022-11-03 13:04:18 +0100

    ***

--- src/actions/save.ts
+++ src/actions/save.ts
@@ -1,24 +1,33 @@
 
 import {supabase} from "@/supabase/index"
+// import { Html3 } from "iconsax-react";
 import type { SaveProps } from "../interfaces"
 // import jwt from "jsonwebtoken"
-export const save = (html:string)=>{
+export const save = async({title,type="html",ref,user_id}:SaveProps)=>{
+  if(ref.current){
+    // remove script tag
+    const html =btoa(ref.current.innerHTML);
 
+      const response = await supabase.from("notes").insert([{
+            title,
+            type,
+            note:html,
+            user_id
+        }])
+      return [response.error,response.data] 
+}
 }
 
 
 // draft - offline
-export const saveDraft = async({title,type="html",ref,user_id}:SaveProps)=>{
+export const saveDraft = async({title,type="html",ref}:SaveProps)=>{
     if(ref.current){
         // remove script tag
         const html =btoa(ref.current.innerHTML);
-
-          const response = await supabase.from("notes").insert([{
-                title,
-                type,
-                note:html,
-                user_id
-            }])
-          return [response.error,response.data] 
+        localStorage.setItem("id:draft",JSON.stringify({
+          title,
+          type,
+          html
+        }))
     }
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,13 +1,22 @@
-import {saveDraft} from "@/actions/save"
-import { useRef, useState} from "react"
+import {saveDraft, save} from "@/actions/save"
+import { useEffect, useRef, useState} from "react"
 import Header from "./Header";
 import Tools from "./Tools";
-
+import * as uuid from "uuid"
+import { redirect, useNavigate } from "react-router-dom";
 const Editor = () => {
   const [title, setTitle] = useState("Untitled")
   const note_editor = useRef<HTMLDivElement>(null!)
-  const onSaveDraft = () :any =>{
-   return saveDraft({
+  const navigate = useNavigate()
+  const onSaveDraft=()=>{
+    return saveDraft({
+      title,
+      ref:note_editor,
+    })
+  }
+
+  const onSave = () :any =>{
+   return save({
       title,
       ref:note_editor,
       user_id:atob(localStorage.getItem("sid") as string)
@@ -15,13 +24,18 @@ const Editor = () => {
     })
   }
 
+  useEffect(()=>{
+    onSaveDraft()
+    console.log("___________-_________________")
+    navigate("/notes/"+title+"-"+uuid.v1()+"-id=12")
+  },[title])
   return (
     <div className="">
       
       <div className="bg-gray-900 px-8">
         <Header title={title} onTitleChange={(e)=> setTitle(e.target.value) } />
       </div>
-      <Tools onSave={onSaveDraft}/>
+      <Tools onSave={onSave}/>
       <div className="editor-wrapper">
         <div className="text-editor px-12 py-6">
           <span ref={note_editor} contentEditable="true"></span>
