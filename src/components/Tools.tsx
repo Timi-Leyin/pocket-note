f78e4074b83813b4a07958cfdcc63cce6d9d01c5
Timileyin <timileyinoyelekan11@gmail.com>
2022-11-26 21:11:04 +0100

    fix: display only when user is logged in

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -11,12 +11,17 @@ import {
   TextalignJustifycenter,
   NoteText,
 } from "iconsax-react";
+import { Action } from "../interfaces";
+import { currentUser } from "@/actions/user";
 
-const Tools = ({ onSave,updated }: { onSave: () => any, updated:string }) => {
+const current = await currentUser()
+console.log(current)
+const Tools = ({ onSave,updated,action }: { onSave: () => any, updated:string,action:Action }) => {
   const [state, setState] = useState<{
     loading?: boolean;
     error?: boolean;
   }>({});
+  console.log(action)
   return (
     <div>
       <header className="bg-gray-800 py-2 items-center px-10 flex justify-between">
@@ -24,7 +29,9 @@ const Tools = ({ onSave,updated }: { onSave: () => any, updated:string }) => {
           <span contentEditable="true">Georama</span>
           <span contentEditable="true">6px</span>
         </div>
-        <div className="flex gap-3 editor-tools">
+        {
+          action != "read" && (
+            <div className="flex gap-3 editor-tools">
           <button onClick={() => format("bold")}>
             <TextBold size="16px" />
           </button>
@@ -55,23 +62,34 @@ const Tools = ({ onSave,updated }: { onSave: () => any, updated:string }) => {
             <TextalignJustifycenter size="16px" />
           </button> */}
         </div>
+          )
+        }
 
-        <div className="">
-          <span className="text-xs mx-1">Updated {updated} </span>
-          <button
+        <div className="">      
+          <span className="text-xs mx-1">{updated && "Updated "+ updated} </span>
+         {
+           action != "read" && (
+            current[1] ? (
+            <button
             className="p-2 text-xs px-7 bg-green-700 rounded-md"
+            disabled={state.loading}
             onClick={async () => {
               setState({ loading: true, error: false });
-              const res = await onSave();
-              if (res[0]) setState({ loading: false, error: true });
-              if (res[1]) setState({ loading: false, error: false });
-              console.clear();
+           const res = !state.loading &&  await onSave();
+              if (typeof res[0] == null) setState({ loading: false, error: true });
+              if (typeof res[1] == null) setState({ loading: false, error: false });
+              setState({...state, loading:false})
+              // console.clear();
+              console.log(res)
             }}
           >
             {state.loading ? "Saving...": !state.error
-              ? "Note Saved" : "Save Note"
+              ? "Note Saved" : state.error?"Failed":"Save Note"
             }
           </button>
+          ) : <span className="text-sm font-bold">Sign in to save note</span>
+           )
+         }
         </div>
       </header>
     </div>
