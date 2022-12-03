6217950da2a55ba7000da0e3c339922fb0fec693
DavidJinng <davidjinng@gmail.com>
2022-12-03 09:25:34 +0100

    Adjusted the Header to some certain level

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,49 +5,83 @@ import { signin } from "@/actions/auth";
 import { useLoader } from "@/hooks/index";
 import { currentUser } from "@/actions/user";
 import Loading from "./Loading";
+import Logo from "./Logo";
 
-const Header = ({ title, onTitleChange,action,uuid, id, isEditable }: HeaderProps) => {
+const Header = ({
+  title,
+  onTitleChange,
+  action,
+  uuid,
+  id,
+  isEditable,
+}: HeaderProps) => {
   const { loading, data } = useLoader(currentUser());
-  const user = data?.user_metadata
+  const user = data?.user_metadata;
   // console.log(user)
-  return (
-    <header className="flex items-center px-2 justify-between p-3 w-full">
-      {/* search */}
-      <Search />
-      { isEditable && (
-        <div className="font-bold text-xl flex-center gap-1">
-        <TicketDiscount />
-      <input type="text" className="opacity-50 bg-transparent outline-none border-none p-1 focus:opacity-100" placeholder="Untitled" onChange={(e:any)=>{
-      if(onTitleChange){
-        onTitleChange(e)
-       action != "new" ?( window.history.replaceState({}, "",`/notes/${e.target.value}=${uuid}=id=${id}`)): window.history.replaceState({}, "",`/notes/${e.target.value}`)
-      }
-      }} value={title} disabled={action === "read"} />
-          </div>
-       )}
 
-      {/*  display loading*/}
-      <div className="">
-        {loading ? (
-          <Loading />
-        ) : user ? (
-          <div className="flex gap-1">
-            {/* show avatar if loading = false && data exists */}
-            <img
-              src={user?.avatar_url}
-              className="w-[50px] h-[50px] ring-2 select-none pointer-events-none ring-gray-100 rounded-full object-cover"
-              alt={user?.full_name}
-            />
-          </div>
-        ) : (
-          // show sign in button if loading = false && there is no current logged user
-          <button
-            className="p-2 bg-red-500 px-7 rounded-full gap-3 flex-center text-xs"
-            onClick={() => signin()}
-          >
-            Sign in <Google variant="Bold" size="16px" />
-          </button>
-        )}
+  return (
+    <header className="w-full h:max-content top-0 left-0 p-3 z-10">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        {/* Logo */}
+        <Logo />
+
+        {/* Search Bar */}
+
+        <div className="hidden md:block md-w:auto">
+          <Search />
+          {isEditable && (
+            <div className="font-bold text-xl flex-center gap-1">
+              <TicketDiscount />
+              <input
+                type="text"
+                className="opacity-50 bg-transparent outline-none border-none p-1 focus:opacity-100"
+                placeholder="Untitled..."
+                onChange={(e: any) => {
+                  if (onTitleChange) {
+                    onTitleChange(e);
+                    action != "new"
+                      ? window.history.replaceState(
+                          {},
+                          "",
+                          `/notes/${e.target.value}=${uuid}=id=${id}`
+                        )
+                      : window.history.replaceState(
+                          {},
+                          "",
+                          `/notes/${e.target.value}`
+                        );
+                  }
+                }}
+                value={title}
+                disabled={action === "read"}
+              />
+            </div>
+          )}
+        </div>
+
+        {/*  Conditional Rendering for Avatar */}
+        <div className="">
+          {loading ? (
+            <Loading />
+          ) : user ? (
+            <div className="flex gap-1">
+              {/* show avatar if loading = false && data exists */}
+              <img
+                src={user?.avatar_url}
+                className="w-[50px] h-[50px] ring-2 select-none pointer-events-none ring-gray-100 rounded-full object-cover"
+                alt={user?.full_name}
+              />
+            </div>
+          ) : (
+            // show sign in button if loading = false && there is no current logged user
+            <button
+              className="p-6 bg-red-500 px-7 rounded-full gap-3 flex-center text-xs"
+              onClick={() => signin()}
+            >
+              Sign in <Google variant="Bold" size="16px" />
+            </button>
+          )}
+        </div>
       </div>
     </header>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,26 +3,31 @@ import { useState } from "react";
 import Loading from "./Loading";
 
 const Search = () => {
-  const [isSearching, setIsSearch] = useState(false)
-  const SearchQuery= ()=>{
-    setIsSearch(true)
-  }
+  const [isSearching, setIsSearch] = useState(false);
+  const SearchQuery = () => {
+    setIsSearch(true);
+  };
   return (
-   <div className="relative">
+    <div className="relative">
       <div className="flex gap-2 items-center p-2 bg-[#242424] rounded-lg">
-      <SearchNormal1 variant="Bulk" size="20px" />
-      <input placeholder="Search ..." onChange={SearchQuery} onBlur={()=>{
-        setIsSearch(false)
-      }} className="bg-transparent outline-none" />
+        <SearchNormal1 variant="Bulk" size="20px" />
+        <input
+          placeholder="Search ..."
+          onChange={SearchQuery}
+          onBlur={() => {
+            setIsSearch(false);
+          }}
+          className="bg-transparent outline-none"
+        />
+      </div>
+      
+      {isSearching && (
+        <div className="p-4 my-1 shadow-sm bg-gray-700 rounded-md absolute w-full  ">
+          <Loading />
+        </div>
+      )}
     </div>
-   {
-     isSearching && (
-      <div className="p-4 my-1 shadow-sm bg-gray-700 rounded-md absolute w-full  ">
-      <Loading />
-    </div>
-     )
-   }
-   </div>
   );
 };
+
 export default Search;
