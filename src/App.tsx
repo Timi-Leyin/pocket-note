6f01d4f730aaa06587c71448cc9b04840e592618
Timileyin <timileyinoyelekan11@gmail.com>
2022-12-11 16:34:20 +0100

    refactor: supabase realtime subscription

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import Note from "@/pages/Note";
+import { useEffect } from "react";
+import { supabase } from "@/supabase/index";
+
 function App() {
+  useEffect(() => {
+    supabase
+      .channel("*")
+      .on("postgres_changes", { event: "*", schema: "*" }, (payload) => {
+        console.log("Change received!", payload);
+      })
+      .subscribe((status) => {
+        console.log(status);
+      });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
