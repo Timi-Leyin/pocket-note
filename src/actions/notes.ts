b70b16db36d28712236773cfb5a8e7bdf8aa8eee
Timileyin <timileyinoyelekan11@gmail.com>
2022-12-11 18:33:03 +0100

    fix: scoop user

--- src/actions/notes.ts
+++ src/actions/notes.ts
@@ -1,17 +1,18 @@
 import { supabase } from "../supabase";
 import { currentUser } from "./user";
 
-const current = await currentUser()
 export const getMyNotes = async ():Promise<[any, any[]]> =>{
+    const current = await currentUser()
     // fix: null readings
     let { data: notes, error } = await supabase.from("notes").select("*").eq("user_id", current[1] && current[1].id || "")
-
+    
     return [error, notes as any[]]
 }
 
 
 
 export const getSharedNotes = async ():Promise<[any, any[]]> =>{
+    const current = await currentUser()
     // fix: null readings
 let { data: notes, error } =  await supabase.from("notes").select("*").contains("shared", current[1] && [current[1].email] || "")
 
