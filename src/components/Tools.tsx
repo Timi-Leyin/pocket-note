c0be92ad41bc5337031d837179b259cee919c6d5
Timileyin <timileyinoyelekan11@gmail.com>
2022-12-13 13:37:35 +0100

    feat: add more tools

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -5,7 +5,8 @@ import {
   TextItalic,
   TextUnderline,
   TextalignRight,
-  Notepad,
+  Book,
+  Ruler,
   TextalignLeft,
   TextalignCenter,
   Eye,
@@ -16,93 +17,125 @@ import { currentUser } from "@/actions/user";
 import { useNavigate } from "react-router-dom";
 import { useLoader } from "../hooks";
 
-const Tools = ({ onSave,updated,action,id, uuid, title }: { onSave: () => any, updated:string,action:Action,id:number, uuid:string; title:string }) => {
+const Tools = ({
+  onSave,
+  updated,
+  action,
+  id,
+  uuid,
+  title,
+}: {
+  onSave: () => any;
+  updated: string;
+  action: Action;
+  id: number;
+  uuid: string;
+  title: string;
+}) => {
   const { data } = useLoader(currentUser());
-  const current = data
+  const current = data;
   const [state, setState] = useState<{
     loading?: boolean;
     error?: boolean;
   }>({});
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div>
-      <header className="bg-gray-800 py-3 items-center gap-3 px-10 flex flex-wrap justify-between">
-      {/* {
-        action == "read"?(
-          <div className="flex gap-2 items-center">
+    <div className="sticky top-0 left-0 w-full ">
+      <header className="bg-gray-800 py-3 items-center gap-3 px-4 md:px-10 flex flex-wrap justify-between">
+        {action == "read" ? (
+          <div className="flex gap-2 items-center text-md">
             <Eye />
             <p>Read-only</p>
           </div>
-        ):(
-          <div className="w-[300px]">
-          <span contentEditable="true">Georama</span>
-          <span contentEditable="true">6px</span>
-        </div>
-        )
-      } */}
-        {
-          action != "read" && (
+        ) : (
+          <div className="">
+            {/* <span contentEditable="true">Georama</span>
+          <span contentEditable="true">6px</span> */}
+          </div>
+        )}
+        {action != "read" && (
+         <div className="overflow-auto w-full">
             <div className="flex gap-3  editor-tools">
-          <button onClick={() => format("bold")}>
-            <TextBold size="16px" />
-          </button>
-          <button onClick={() => format("italic")}>
-            <TextItalic size="16px" />
+            <button onClick={() => format("bold")}>
+              <TextBold size="16px" />
+            </button>
+            <button onClick={() => format("italic")}>
+              <TextItalic size="16px" />
+            </button>
+
+            <button onClick={() => format("underline")}>
+              <TextUnderline size="16px" />
+            </button>
+
+            <button onClick={() => format("justifyLeft")}>
+              <TextalignLeft size="16px" />
+            </button>
+
+            <button onClick={() => format("justifyCenter")}>
+              <TextalignCenter size="16px" />
+            </button>
+
+            <button onClick={() => format("justifyRight")}>
+              <TextalignRight size="16px" />
+            </button>
+
+            <button onClick={() => format("insertOrderedList")}>
+            <Book size="16px" />
           </button>
 
-          <button onClick={() => format("underline")}>
-            <TextUnderline size="16px" />
-          </button>
-
-          <button onClick={() => format("justifyRight")}>
-            <TextalignRight size="16px" />
-          </button>
-
-          <button onClick={() => format("justifyLeft")}>
-            <TextalignLeft size="16px" />
-          </button>
-
-          <button onClick={() => format("justifyCenter")}>
-            <TextalignCenter size="16px" />
-          </button>
-
-          {/* <button onClick={() => format("insertOrderedList")}>
+            <button onClick={() => format("insertUnorderedList")}>
             <NoteText size="16px" />
-          </button> */}
-          {/* <button onClick={() => format("justify")}>
+          </button>
+
+            <button onClick={() => format("InsertHorizontalRule")}>
+            <Ruler size="16px" />
+          </button>
+            {/* <button onClick={() => format("justify")}>
             <TextalignJustifycenter size="16px" />
           </button> */}
-        </div>
-          )
-        }
+          </div>
+         </div>
+        )}
 
-        <div className="">      
-          <span className="text-xs mx-1">{updated && "Updated "+ updated} </span>
-         {
-           action != "read" && (
-            current ? (
-            <button
-            className="p-2 text-xs px-7 bg-green-700 rounded-md"
-            disabled={state.loading}
-            onClick={async () => {
-              setState({ loading: true, error: false });
-           const res = !state.loading &&  await onSave();
-          //  console.log(res)
-              if (typeof res[0] == null) setState({ loading: false, error: true });
-              if (typeof res[1] == null) setState({ loading: false, error: false });
-              setState({...state, loading:false});
-             (!state.error && !state.loading && action=="new" ) && window.location.replace(`/notes/${title}=${uuid}=id=${id}`)
-            // console.log(window.history);
-            //  (!state.error && !state.loading && action=="new" ) && window.history.replaceState({}, "",`/notes/${title}=${uuid}=id=${id}`)
-            }}
-          >
-            {state.loading ? "Saving...": !state.error
-              ? "Note Saved" : state.error?"Failed":"Save Note"
-            }
-          </button>
-          ) : <span className="text-sm font-bold py-2">Sign in to save note</span>
-           )
-         }
+        <div className="">
+          <span className="text-xs mx-1">
+            {updated && "Updated " + updated}{" "}
+          </span>
+          {action != "read" &&
+            (current ? (
+              <button
+                className="p-2 text-xs px-7 bg-green-700 rounded-md"
+                disabled={state.loading}
+                onClick={async () => {
+                  setState({ loading: true, error: false });
+                  const res = !state.loading && (await onSave());
+                  //  console.log(res)
+                  if (typeof res[0] == null)
+                    setState({ loading: false, error: true });
+                  if (typeof res[1] == null)
+                    setState({ loading: false, error: false });
+                  setState({ ...state, loading: false });
+                  !state.error &&
+                    !state.loading &&
+                    action == "new" &&
+                    window.location.replace(`/notes/${title}=${uuid}=id=${id}`);
+                  // console.log(window.history);
+                  //  (!state.error && !state.loading && action=="new" ) && window.history.replaceState({}, "",`/notes/${title}=${uuid}=id=${id}`)
+                }}
+              >
+                {state.loading
+                  ? "Saving..."
+                  : !state.error
+                  ? "Note Saved"
+                  : state.error
+                  ? "Failed"
+                  : "Save Note"}
+              </button>
+            ) : (
+              <span className="text-sm font-bold py-2">
+                Sign in to save note
+              </span>
+            ))}
         </div>
       </header>
     </div>
