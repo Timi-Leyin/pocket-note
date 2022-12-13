d9ede2400adb718e3d6884abe0c9c98bfb30b74e
Timileyin <timileyinoyelekan11@gmail.com>
2022-12-13 13:37:09 +0100

    fix: button sizes

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -12,6 +12,8 @@ import { decrypt } from "@/utils/hash";
 const Note = ({ data }:{data:NoteProps}) => {
    const user = useLoader(currentUser());
   const current = user.data;
+  const loading = user.loading
+
   const [isLoading, setIsLoading] = useState(false)
   // console.log(data)
   const element= document.createElement("p")
@@ -33,21 +35,22 @@ const Note = ({ data }:{data:NoteProps}) => {
           </p>
     <div className="flex items-center gap-3">
     <Link to={`/notes/${data.title}=${data.uuid}=id=${data.id}`}>
-          <div className="w-[30px] mx-2 gap-2 flex-center text-black cursor-pointer  h-[30px] rounded-full bg-white">
-            {current && current.id == data.user_id ? <Edit size="13px" /> : <Eye size="13px"/>}
+          <div className="w-[50px] mx-2 gap-2 flex-center text-black cursor-pointer  h-[50px] rounded-full bg-white">
+            {loading ? <Loading />: current.id === data.user_id ? <Edit size="18px" />: <Eye size="18px"/>}
+            {/* {current && current.id == data.user_id ? <Edit size="18px" /> : <Eye size="18px"/>} */}
           </div>
           </Link>
           {
-            (current && current.id ) == data.user_id && (
-              <div className="w-[30px] mx-2 flex-center text-black cursor-pointer  h-[30px] rounded-full bg-red-500" onClick={async()=>{
+           loading? <Loading />: (current && current.id ) == data.user_id ? (
+              <div className="w-[50px] mx-2 flex-center text-black cursor-pointer  h-[50px] rounded-full bg-red-500" onClick={async()=>{
                 setIsLoading(true)
                 const response = await supabase.from("notes").delete().eq("uuid",data.uuid)
                 // console.log(response)
             if(!response.error) window.location.reload()
              }}>
-              {isLoading ? <Loading /> : <Trash size="13px" />} 
+              {isLoading ? <Loading /> : <Trash size="18px" />} 
              </div>
-            )
+            ): null
           }
          
     </div>
